d79b2f2fdbb3cfaca699984083d6f630caebbdfe
lirenkaidong.lkd@antgroup.com <kaidong.lkd@antgroup.com>
2023-10-24 23:07:55 +0800

    add extractBizInfoFromBizUrl

--- sofa-serverless-runtime/arklet-core/src/main/java/com/alipay/sofa/serverless/arklet/core/command/builtin/handler/InstallBizHandler.java
+++ sofa-serverless-runtime/arklet-core/src/main/java/com/alipay/sofa/serverless/arklet/core/command/builtin/handler/InstallBizHandler.java
@@ -33,16 +33,25 @@ import lombok.Setter;
 import java.lang.management.ManagementFactory;
 import java.lang.management.MemoryPoolMXBean;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 /**
  * @author mingmen
  * @date 2023/6/8
  */
 public class InstallBizHandler
-                              extends
-                              AbstractCommandHandler<InstallBizHandler.Input, InstallBizHandler.InstallBizClientResponse>
-                                                                                                                         implements
-                                                                                                                         ArkBizOps {
+        extends
+        AbstractCommandHandler<InstallBizHandler.Input, InstallBizHandler.InstallBizClientResponse>
+        implements
+        ArkBizOps {
+
+    private final static String ROOT_WEB_CONTEXT_PATH = "/";
+
+    private final static String DASH                  = "-";
+
+    private final static String BIZ_VERSION_REGEX     = "\\d+\\.\\d+\\.\\d+(-SNAPSHOT)?";
 
     @Override
     public Output<InstallBizClientResponse> handle(Input input) {
@@ -50,9 +59,9 @@ public class InstallBizHandler
         long startSpace = metaSpaceMXBean.getUsage().getUsed();
         try {
             InstallBizClientResponse installBizClientResponse = convertClientResponse(getOperationService()
-                .install(input.getBizUrl()));
+                    .install(input.getBizUrl()));
             installBizClientResponse.setElapsedSpace(metaSpaceMXBean.getUsage().getUsed()
-                                                     - startSpace);
+                    - startSpace);
             if (ResponseCode.SUCCESS.equals(installBizClientResponse.getCode())) {
                 return Output.ofSuccess(installBizClientResponse);
             } else {
@@ -80,6 +89,57 @@ public class InstallBizHandler
         return installBizClientResponse;
     }
 
+    private Input extractBizInfoFromBizUrl(Input input) {
+        final String bizUrl = input.getBizUrl();
+        if (StringUtils.isEmpty(bizUrl)) {
+            return input;
+        }
+
+        Input extractedInput = new Input();
+        extractedInput.setBizUrl(input.getBizUrl());
+        extractedInput.setBizName(input.getBizName());
+        extractedInput.setBizVersion(input.getBizVersion());
+        extractedInput.setRequestId(input.getRequestId());
+        extractedInput.setAsync(input.isAsync());
+
+        // 根据 bizUrl 解析出 bizName 和 bizVersion
+        String[] bizUrlSplit = bizUrl.split(ROOT_WEB_CONTEXT_PATH);
+        if (bizUrlSplit.length == 0) {
+            return extractedInput;
+        }
+        String packageName = bizUrlSplit[bizUrlSplit.length - 1];
+        // 对 bizNameAndVersion 进行 bizVersionRegex 正则匹配，如果匹配，取出来作为 bizVersion
+        Pattern pattern;
+        try {
+            pattern = Pattern.compile(BIZ_VERSION_REGEX);
+        } catch (PatternSyntaxException patternSyntaxException) {
+            return extractedInput;
+        }
+        Matcher matcher = pattern.matcher(packageName);
+        String bizVersion = null;
+        if (matcher.find()) {
+            bizVersion = matcher.group();
+        }
+        if (StringUtils.isEmpty(bizVersion)) {
+            return extractedInput;
+        }
+        // 如果bizVersion非空，将 packageName 按 bizVersion 进行分割，取前面的部分作为 bizName
+        String bizName = packageName.split(bizVersion)[0];
+        if (StringUtils.isEmpty(bizName)) {
+            return extractedInput;
+        }
+        // 将bizName尾部的 - 去掉
+        if (bizName.endsWith(DASH)) {
+            bizName = bizName.substring(0, bizName.length() - 1);
+        }
+        if (!StringUtils.isEmpty(bizName)) {
+            // 只有模块名和版本号都解析到了才进行抽取覆盖
+            extractedInput.setBizName(bizName);
+            extractedInput.setBizVersion(bizVersion);
+        }
+        return extractedInput;
+    }
+
     @Override
     public Command command() {
         return BuiltinCommand.INSTALL_BIZ;
@@ -87,11 +147,15 @@ public class InstallBizHandler
 
     @Override
     public void validate(Input input) throws CommandValidationException {
+        notBlank(input.getBizUrl(), "bizUrl should not be blank");
+
+        // 如果能根据 bizUrl 解析出 bizName 和 bizVersion，则用解析出来的覆盖原本输入的
+        input = extractBizInfoFromBizUrl(input);
+
         notBlank(input.getBizName(), "bizName should not be blank");
         notBlank(input.getBizVersion(), "bizVersion should not be blank");
         isTrue(!input.isAsync() || !StringUtils.isEmpty(input.getRequestId()),
-            "requestId should not be blank when async is true");
-        notBlank(input.getBizUrl(), "bizUrl should not be blank");
+                "requestId should not be blank when async is true");
     }
 
     @Getter
